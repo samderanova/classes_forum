36006cb28491ed12029f72aedd91f98f604df4b7
Sam Der <samder2468@gmail.com>
2020-10-19 22:23:26 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Router, Route} from 'react-router';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
