0c62d72debd884272477e01add6181a8aef2dd24
Joseph Kim <josephkim1444@gmail.com>
2020-10-20 23:23:05 -0700

    created header and footer component files

--- client/src/components/Footer.component.js
+++ client/src/components/Footer.component.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 export default class Footer extends Component {
     render(){
-        
+        return (
+            <div></div>
+        )
     }
 }
